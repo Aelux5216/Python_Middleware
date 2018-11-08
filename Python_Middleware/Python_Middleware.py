f99b3eec065a85cb789ae0fa065111756614bfe3
Aelux5216 <thedragongamers@gmail.com>
2018-11-08 22:36:25 +0000

    service class testing(use breakpoint to view result)

--- Python_Middleware/Python_Middleware.py
+++ Python_Middleware/Python_Middleware.py
@@ -11,6 +11,67 @@ def initSession():
     newSession = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01", api_key="***REMOVED***")
     return newSession
 
+##Class creation##
+
+class Service:
+    def __init__(self, service_no, dep_code, arr_code):
+        
+        session = initSession()
+
+        depBoardRequest = session.get_station_board(dep_code,destination_crs=arr_code)
+        service = depBoardRequest.train_services[service_no]
+        depServiceId = service.service_id
+        depServiceDet = session.get_service_details(depServiceId)
+        arrDestPoints = depServiceDet.subsequent_calling_points
+        
+        for e in arrDestPoints:
+            if arr_code in e.crs:
+                arrDest = e
+            pass
+
+        arrBoardRequest = session.get_station_board(arr_code,origin_crs=dep_code,include_departures=False, include_arrivals=True)
+
+        for a in depBoardRequest.train_services:
+            if depServiceId in a.service_id:
+                arr_platform = a.platform
+            pass
+
+        self.service_id = service.service_id
+        self.operator = service.operator_name
+        self.dep_name = service.origin_text
+        self.dep_code = depServiceDet.crs
+        self.dep_time = service.std
+        self.dep_platform = service.platform
+        self.arr_name = arrDest.location_name
+        self.arr_code = arr_code
+        self.arr_time = arrDest.st
+        self.arr_platform = arr_platform
+        self.status = service.etd
+        self.disrupt_reason = depServiceDet.disruption_reason 
+        self.calls_at = []
+
+        for b in arrDestPoints:
+            p1 = Calling_Points(b.location_name,b.crs,b.st,b.et)
+            self.calls_at.append(copy.copy(p1))
+
+        self.stops = len(self.calls_at)
+
+class Calling_Points:
+    def __init__(self, name, code, time, status):
+        
+        self.name = name
+        self.code = code
+        self.time = time
+        self.status = status
+
+class Tickets:
+    def __init__(self, reference, ticket_type, cost):
+
+        self.reference = reference
+        self.ticket_type = ticket_type
+        self.cost = cost
+
+
 #Create instance that allows me to enter commands
 class Handle_Data(asyncore.dispatcher_with_send):
 
@@ -34,7 +95,7 @@ class Handle_Data(asyncore.dispatcher_with_send):
             for a in boardRequest.train_services:
                 arrStations.append(a.destination_text)
                 
-            formatted = "{".join(arrStations)
+            formatted = "{".join(arrStations) #Use this to format station calling points later.
 
             print(formatted)
 
@@ -71,60 +132,9 @@ class Server(asyncore.dispatcher):
 server = Server('0.0.0.0', 8000) #Create new instance of server, 0.0.0.0 means it will host on any port so both 127.0.0.1(local pc only) and 192.168.1.1 will be hosted so it can be accessed from other pcs.
 print("listening..") #State that the server is listening again. 
 
-##Class creation##
-
-class Service:
-    def __init__(self, service_no, dep_code, arr_code):
-        
-        session = initSession()
-
-        depBoardRequest = session.get_station_board(dep_code,destination_crs=arr_code)
-        service = depBoardRequest.train_services[service_no]
-        
-        depServiceDet = session.get_service_details(depService_id)
-        arrDestPoints = depServiceDet.subsequent_calling_points
-        arrDest = arrDestPoints[len(arrDestPoints) - 1]
-
-        arrBoardRequest = session.get_station_board(arr_code,origin_crs=dep_code,include_departures=False, include_arrivals=True)
-
-        for a in depBoardRequest.train_services:
-            if depService_id in a.service_id:
-                arr_Platform = a.platform
-            pass
-
-        self.service_id = service.service_id
-        self.operator = service.operator_name
-        self.dep_name = service.origin_text
-        self.dep_code = dep_code
-        self.dep_time = service.std
-        self.dep_platform = service.platform
-        self.arr_name = service.destination_text
-        self.arr_code = arr_code
-        self.arr_time = arrDest.st
-        self.arr_platform = arr_platform
-        self.status = service.etd
-        self.disrupt_reason = depServiceDet.disruption_reason
-        self.calls_at 
-        self.stops = calls_at.count()
-
-        for b in arrDestPoints:
-            p1 = Calling_Points(b.location_name,b.crs,b.st,et)
-            self.calls_at.append(copy.copy(p1))
-            p1.reset()
-
-class Calling_Points:
-    def __init__(self, name, code, time, status):
-        
-        self.name = name
-        self.code = code
-        self.time = time
-        self.status = status
-
-class Tickets:
-    def __init__(self, reference, ticket_type, cost):
-
-        self.reference = reference
-        self.ticket_type = ticket_type
-        self.cost = cost
+indep = input("(crs code)Departing from?")
+indest = input("(crs code)Going to?")
+#Will use selected value service for service no, for now defaulting to first service index of 0
+service1 = Service(0,indep,indest)
 
 asyncore.loop() #Call loop method of asyncore to begin listening for clients again.
