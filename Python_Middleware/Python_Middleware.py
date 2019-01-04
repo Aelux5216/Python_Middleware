9f5bf98ff0c33e091509e3d8b7ccd20d6edffff5
Aelux5216 <thedragongamers@gmail.com>
2019-01-04 18:12:40 +0000

    Added getOne method and moved string splitting from website

--- Python_Middleware/Python_Middleware.py
+++ Python_Middleware/Python_Middleware.py
@@ -44,24 +44,18 @@ def GetAll(dep_code, arr_code):
                     if arr_code in e.crs:
                         arrDest = e
                     pass
-                
-                for a in depBoardRequest.train_services:
-                    if depServiceId in a.service_id:
-                        arr_platform = a.platform
-                    pass
 
                 service1 = Service()
 
                 service1.service_id = y.service_id
                 service1.operator = y.operator_name
-                service1.dep_name = y.origin_text
+                service1.dep_name = depBoardRequest.location_name
                 service1.dep_code = depServiceDet.crs
                 service1.dep_time = y.std
                 service1.dep_platform = y.platform
                 service1.arr_name = arrDest.location_name
                 service1.arr_code = arr_code
                 service1.arr_time = arrDest.st
-                service1.arr_platform = arr_platform
                 service1.status = y.etd
                 service1.disrupt_reason = depServiceDet.disruption_reason 
                 service1.calls_at = []
@@ -83,6 +77,65 @@ def GetAll(dep_code, arr_code):
     else:
         return error
 
+def GetOne(serviceNo,arrCode):
+    session = None
+    service = None
+    error = ""
+    
+    try:
+        session = initSession()
+
+    except:
+        error = "NoConn"
+
+    if session is not None:
+        try:
+            service = session.get_service_details(serviceNo)
+        except:
+            pass
+        if service is not None :
+            
+            arrDestPoints = service.subsequent_calling_points
+            
+            for e in arrDestPoints:
+                    if arrCode in e.crs:
+                        arrDest = e
+                    pass
+
+            service1 = Service()
+
+            service1.operator = service.operator_name
+            service1.dep_name = service.location_name
+            service1.dep_code = service.crs
+            service1.dep_time = service.std
+            service1.dep_platform = service.platform
+            service1.arr_name = arrDest.location_name
+            service1.arr_code = arrCode
+            service1.arr_time = arrDest.st
+            service1.status = service.etd
+            service1.disrupt_reason = service.disruption_reason 
+            service1.calls_at = []
+
+            for b in arrDestPoints:
+                p1 = Calling_Points(b.location_name,b.crs,b.st,b.et)
+                p1s = json.dumps(vars(p1))
+                service1.calls_at.append(str(p1s))
+
+            service1.stops = len(service1.calls_at)
+
+            jsonObject = json.dumps(vars(service1))
+
+            allServices = []
+
+            allServices.append(str(jsonObject))
+
+            return allServices
+        else:
+            error = "NoServices"
+            return error
+    else:
+        return error
+
 class Calling_Points:
     def __init__(self, name, code, time, status):
         
@@ -91,15 +144,7 @@ class Calling_Points:
         self.time = time
         self.status = status
 
-class Tickets:
-    def __init__(self, reference, ticket_type, cost):
-
-        self.reference = reference
-        self.ticket_type = ticket_type
-        self.cost = cost
-
-
-#Create instance that allows me to enter commands
+#Create instance that allows it to recieve commands
 class Handle_Data(asyncore.dispatcher_with_send):
 
     def handle_read(self):
@@ -109,23 +154,37 @@ class Handle_Data(asyncore.dispatcher_with_send):
 
         if "GetAll" in decodedData:
             
-            session = initSession()
-
             splitData = decodedData.split("{")
             
-            depStation = splitData[1]
+            depStation = (splitData[1])[-4:].strip(')')
 
-            arrStation = splitData[2]
+            arrStation = (splitData[2])[-4:].strip(')')
             
             result = GetAll(depStation,arrStation)
 
-            if "NoServices" not in result and "NoConn" not in result:
-                
+            if result != "NoServices" and result != "NoConn":
                 jsonObject = json.dumps(result)
                 self.send(jsonObject.encode())
 
             else:
+                self.send(result.encode())
 
+        elif "GetOne" in decodedData:
+
+            splitData = decodedData.split("{")
+
+            serviceNo = splitData[1]
+
+            arrCode = splitData[2]
+
+            #Will use selected value service for service no, for now defaulting to first service index of 0
+            result = GetOne(serviceNo,arrCode)
+
+            if result != "NoServices" and result != "NoConn":
+                jsonObject = json.dumps(result)
+                self.send(jsonObject.encode())
+
+            else:
                 self.send(result.encode())
         else:
             pass
