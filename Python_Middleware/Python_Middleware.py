f24a92544d0a17e6adfe2da3e1cc601acf5dde74
Aelux5216 <thedragongamers@gmail.com>
2019-06-08 17:40:15 +0100

    remove strings

--- Python_Middleware/Python_Middleware.py
+++ Python_Middleware/Python_Middleware.py
@@ -8,7 +8,7 @@ from nredarwin.webservice import DarwinLdbSession
 
 #Connect to API
 def initSession():
-    newSession = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01", api_key=***REMOVED***)
+    newSession = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01", api_key="***REMOVED***")
     return newSession
 
 ##Class creation##
